bf5e6663b7c49d1b99dd7bd432698c1563441576
ula0210 <ulanowak0210@gmail.com>
2026-05-30 13:50:44 +0200

    fix: display scientific plant name

--- frontend/src/components/PlantProfile/PlantProfile.tsx
+++ frontend/src/components/PlantProfile/PlantProfile.tsx
@@ -19,7 +19,7 @@ export function PlantProfile() {
             </p>
 
             <p>
-              <strong>Scientific name:</strong> {plant.commonName}
+              <strong>Scientific name:</strong> {plant.scientificName}
             </p>
 
             <p>
